d4ff3481a5b41b19f0279ed1c1801e4e475b26b4
Rachel Schefke <rschefke@pal-nat186-74-119.itap.purdue.edu>
2016-04-24 14:01:34 -0400

    fixed Spark

--- src/main/java/startr/Servlet.java
+++ src/main/java/startr/Servlet.java
@@ -2,33 +2,22 @@ package startr;
 
 import static spark.Spark.get;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet("/")
-public class Servlet extends HttpServlet {
+public class Servlet implements spark.servlet.SparkApplication {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -2344357533926691472L;
 
 	@Override
-	public void init() throws ServletException {
-		super.init();
-		
+	public void init() {
+
 		get("/hello", (req, res) -> "Hello World");
 	}
-	
+
 	public static void main(String[] args) {
 		Servlet s = new Servlet();
-		
-		try {
-			s.init();
-		} catch (ServletException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		s.init();
+
 	}
 }
