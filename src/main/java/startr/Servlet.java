e184ad86d26dbda94566fc1d7bec7571c1064be9
Rachel Schefke <rschefke@pal-nat186-74-119.itap.purdue.edu>
2016-04-24 19:02:29 -0400

    static file serving

--- src/main/java/startr/Servlet.java
+++ src/main/java/startr/Servlet.java
@@ -2,21 +2,29 @@ package startr;
 
 import static spark.Spark.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.google.gson.Gson;
 import startr.StartUp.Logo;
 
-public class Servlet implements spark.servlet.SparkApplication {
-
-	/**
-	 * 
-	 */
+public class Servlet extends spark.servlet.SparkFilter implements spark.servlet.SparkApplication {
 
 	@Override
 	public void init() {
 
 		//get("/hello", (req, res) -> "Hello World");
+		//staticFileLocation("/src/main/webapp");
 		
 		Gson gson = new Gson();
 
@@ -39,6 +47,24 @@ public class Servlet implements spark.servlet.SparkApplication {
 			
 			return result;
 		}, gson::toJson);
+	}
+	
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String requestUrl = ((HttpServletRequest) req).getRequestURI().toString();
+		
+		Map<String, String> mimeMapping = new HashMap<>();
+        mimeMapping.put(".css","text/css");
+        mimeMapping.put(".js","text/javascript");
+
+        for(Map.Entry<String,String> entry : mimeMapping.entrySet())
+        {
+            if(requestUrl.endsWith(entry.getKey()))
+            {
+                ((HttpServletResponse) res).setHeader("Content-Type",entry.getValue());
+            }
+        }
+        super.doFilter(req, res, chain);
 	}
 
 	public static void main(String[] args) {
