97c037aaf12586f9e65888be9afb4f1fec0cd76a
Rachel Schefke <rschefke@pal-nat186-74-119.itap.purdue.edu>
2016-04-24 16:35:34 -0400

    added FormData

--- src/main/java/startr/Servlet.java
+++ src/main/java/startr/Servlet.java
@@ -1,6 +1,10 @@
 package startr;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
+
+import java.util.List;
+
+import com.google.gson.Gson;
 
 public class Servlet implements spark.servlet.SparkApplication {
 
@@ -11,7 +15,24 @@ public class Servlet implements spark.servlet.SparkApplication {
 	@Override
 	public void init() {
 
-		get("/hello", (req, res) -> "Hello World");
+		//get("/hello", (req, res) -> "Hello World");
+		
+		Gson gson = new Gson();
+
+		post("/processData", (req, res) -> {
+			
+			
+			FormData fd = gson.fromJson(req.body(), FormData.class);
+			
+			List<String> target = fd.getTarget();
+			List<String> category = fd.getCategory();
+			List<String> keywords = fd.getKeywords();
+			
+			StartUp start = new StartUp(target, category, keywords);
+			
+			
+			return fd;
+		}, gson::toJson);
 	}
 
 	public static void main(String[] args) {
