b85c709d469a9c8cd4a5b14b64bb7d8c78bb1943
Rachel Schefke <rschefke@pal-nat186-74-119.itap.purdue.edu>
2016-04-24 20:31:41 -0400

    fixed keyword mappings

--- src/main/java/startr/FormData.java
+++ src/main/java/startr/FormData.java
@@ -15,7 +15,43 @@ public class FormData {
 	public static final Map<String, String> map;
 	static {
 		map = new HashMap<String, String>();
-		//map.put();
+		map.put("kids", "Kids");
+		map.put("teens", "Teens");		
+		map.put("adults", "Adults");
+		map.put("elderly", "Elderly");
+		map.put("travel", "Travel");
+		map.put("transportation", "Transportation");
+		map.put("lodging", "Lodging");
+		map.put("attractions", "Attractions");
+		map.put("fitness", "Fitness");
+		map.put("running", "Running");
+		map.put("diet", "Diet");
+		map.put("workout", "Workout");
+		map.put("social", "Social Networking");
+		map.put("chat", "Chat");
+		map.put("professional", "Professional");
+		map.put("dating", "Dating");
+		map.put("productivity", "Productivity");
+		map.put("note", "Note");
+		map.put("list", "List");
+		map.put("calendar", "Calendar");
+		map.put("finance", "Finance");
+		map.put("banking", "Banking");
+		map.put("investments", "Investments");
+		map.put("budget", "Budget");
+		map.put("shopping", "Shopping");
+		map.put("clothing", "Clothing & Accessories");
+		map.put("home", "Home Goods");
+		map.put("electronics", "Electronics");
+		map.put("medai", "Media");
+		map.put("music", "Music");
+		map.put("video", "Videos");
+		map.put("photo", "Photos");
+		map.put("education", "Education");
+		map.put("study", "Study");
+		map.put("learn", "Learn");
+		map.put("reference", "Reference");
+
 	}
 	
 	public List<String> getTarget() {
