8e8c66861130b2e67434f4d7544f6b4628bc9c63
Rachel Schefke <rschefke@pal-nat186-74-119.itap.purdue.edu>
2016-04-24 22:23:49 -0400

    roll our own http

--- src/main/java/startr/CommentAnalyzer.java
+++ src/main/java/startr/CommentAnalyzer.java
@@ -1,43 +1,66 @@
 package startr;
 
-import com.ibm.watson.developer_cloud.tone_analyzer.v3_beta.ToneAnalyzer;
-import com.ibm.watson.developer_cloud.tone_analyzer.v3_beta.model.ToneAnalysis;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.google.gson.Gson;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class CommentAnalyzer {
 
 	public String text;
 	public double score;
-	
+
 	public CommentAnalyzer(String _text) {
 		text = _text;
 	}
-	
-	public String analyze() {
-		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_02_11);
-		
-		service.setUsernameAndPassword("7f57a9af-0c6a-4731-a2ea-adbd5ffd4ecc", "QlwdyAT7Exfs");
-		
-		ToneAnalysis tone = service.getTone(text).execute();
-		
-		tone.getDocumentTone().getTones().forEach(value -> {
-			value.getTones().forEach(t -> {
-				if(t.getName().equals("Confident")) {
-					score = t.getScore();
-				}
-			});
-		});
-				
-		String analysis = "Your Tone Confidence score is " + score + ". ";
-		
-		if(score > 0.5) {
-			analysis+="Congratulations! This means that you have great confidence with your startup, which means you are on the road to success! I am sure that this startup will be the next hottest company. You have the gumption to do great in this dog eat dog startup world. Keep up the great work!";
-		}
-		else {
-			analysis+="This means that you are not confident in your startup.  Being confident will help you get more money along the way of your startup journey, so you should really start to believe in yourself and your million dollar idea. Don't give up!";
-		}
-		
-		return analysis;
-		
+
+	public HttpResponse<JsonNode> doHTTP(String str) throws UnirestException {
+		Gson g = new Gson();
+
+		String body = "{\"text\": " + g.toJson(str, String.class) + "}";
+
+		return Unirest.post("https://gateway.watsonplatform.net/tone-analyzer-beta/api/v3/tone?version=2016-02-11")
+				.basicAuth("7f57a9af-0c6a-4731-a2ea-adbd5ffd4ecc", "QlwdyAT7Exfs")
+				.header("Content-Type", "application/json")
+				.body(body)
+				.asJson();
 	}
-	
+
+	public String analyze() {
+		JsonNode res;
+		score = 0.0f;
+
+		try {
+			res = doHTTP(text).getBody();
+			score = res.getObject()
+					.getJSONObject("document_tone")
+					.getJSONArray("tone_categories")
+					.getJSONObject(1)
+					.getJSONArray("tones")
+					.getJSONObject(1)
+					.getDouble("score");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String analysis = "Your Tone Confidence score is " + score + ". ";
+
+		if (score > 0.5) {
+			analysis += "Congratulations! This means that you have great confidence with your startup, which means you are on the road to success! I am sure that this startup will be the next hottest company. You have the gumption to do great in this dog eat dog startup world. Keep up the great work!";
+		} else {
+			analysis += "This means that you are not confident in your startup.  Being confident will help you get more money along the way of your startup journey, so you should really start to believe in yourself and your million dollar idea. Don't give up!";
+		}
+
+		return analysis;
+
+	}
+
 }
